430df035e868af83c88995ee19e77b21610fa175
Mostafa Abu El-Khair <mabuelkhair94@gmail.com>
2020-03-20 20:54:21 +0200

    handle receving message

--- app/services/telegram/recevier_service.rb
+++ app/services/telegram/recevier_service.rb
@@ -6,16 +6,24 @@ class Telegram::RecevierService < ApplicationService
   end
 
   def call
+    # puts @message.inspect
+    sender = create_or_update_sender
+    conversation = create_or_update_conversation
+    message = create_or_update_message(sender, conversation)
+  end
+
+  def create_or_update_sender
     # get sender and update name if changed (should use upsert but sqlite does not support it)
-    puts @message.inspect
     sender = Sender.find_or_create_by(id: @message.from.id)
     if (sender.first_name != @message.from.first_name or 
         sender.last_name != @message.from.last_name)
-      sender.first_name = @message.from.first_name 
       sender.last_name = @message.from.last_name
       sender.save
     end
-    # get conversation and update title (should use upsert)
+    sender
+  end
+
+  def create_or_update_conversation
     conversation = Conversation.find_or_create_by(conversation_id: @message.chat.id, 
       conversation_type: @message.chat.type)
     if conversation.conversation_type != 'private' && conversation.title != @message.chat.title
@@ -25,5 +33,19 @@ class Telegram::RecevierService < ApplicationService
         conversation.title = sender.full_name
         conversation.save
     end
+    conversation
+  end
+
+  def create_or_update_message(sender, conversation)
+    # get message and update content if changed (use upsert if not sqlite)
+    message = Message.find_or_create_by(id: @message.message_id, 
+      sender_id: sender.id,
+      conversation_id: conversation.id
+      )
+    if message.content != @message.text
+      message.content = @message.text
+      message.save
+    end
+    message
   end
 end

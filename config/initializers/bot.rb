51ad497b1d7686667930f2de4596298e37711e35
Mostafa Abu El-Khair <mabuelkhair94@gmail.com>
2020-03-20 23:33:49 +0200

    run bot initialzer if database table created

--- config/initializers/bot.rb
+++ config/initializers/bot.rb
@@ -4,8 +4,10 @@ token = Rails.application.credentials.telegram[:bot_token]
 
 Telegram::Bot::Client.run(token) do |bot|
   response = bot.api.getMe()
-  Sender.find_or_create_by(id: response["result"]["id"],
-    first_name: response["result"]["first_name"])
-  # TODO: handle old messages and conversation if new bot used
+  if ActiveRecord::Base.connection.table_exists? Sender.table_name
+    Sender.find_or_create_by(id: response["result"]["id"],
+      first_name: response["result"]["first_name"])
+    # TODO: handle old messages and conversation if new bot used
+  end
 
 end

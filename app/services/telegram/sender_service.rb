770a06ed455dbbcd31c77ea0045a1193c549991b
Mostafa Abu El-Khair <mabuelkhair94@gmail.com>
2020-03-20 23:36:16 +0200

    save sent messages

--- app/services/telegram/sender_service.rb
+++ app/services/telegram/sender_service.rb
@@ -11,7 +11,12 @@ class Telegram::SenderService < ApplicationService
   def call
     token = Rails.application.credentials.telegram[:bot_token]
     Telegram::Bot::Client.run(token) do |bot|
-      bot.api.send_message(chat_id: chat_id, text: message)
+      response = bot.api.send_message(chat_id: chat_id, text: message)
+      # Todo: handle exceptions in message sending
+      puts "#{Message.create(id: response["result"]["message_id"],
+        sender_id: response["result"]["from"]["id"],
+        conversation_id: chat_id, content: message).errors.inspect}"
     end
   end
+
 end

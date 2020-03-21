87a2f8c54e64dc8b9543121a37e852cbce604370
Mostafa Abu El-Khair <mabuelkhair94@gmail.com>
2020-03-21 19:53:14 +0200

    init testing data

--- test/controllers/conversations_controller_test.rb
+++ test/controllers/conversations_controller_test.rb
@@ -2,8 +2,10 @@ require 'test_helper'
 
 class ConversationsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get conversations_index_url
+    get '/conversations'
     assert_response :success
+    assert_equal Conversation.all, assigns(:conversations)
+    assert_equal Conversation.count, assigns(:conversations).length
   end
 
 end

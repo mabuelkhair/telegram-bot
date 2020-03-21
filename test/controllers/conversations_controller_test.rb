e1a33bcd4ad16c8b0d2e9f565751954f24368816
Mostafa Abu El-Khair <mabuelkhair94@gmail.com>
2020-03-21 23:05:55 +0200

    add some tests

--- test/controllers/conversations_controller_test.rb
+++ test/controllers/conversations_controller_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+  BOT_ID = 55
 
 class ConversationsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
@@ -6,6 +7,21 @@ class ConversationsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_equal Conversation.all, assigns(:conversations)
     assert_equal Conversation.count, assigns(:conversations).length
+  end
+
+  test "should get conversation and conversations" do
+    id = Conversation.first.id
+    get '/conversations/' + id.to_s
+    assert_response :success
+    assert_equal Conversation.find(id), assigns(:conversation)
+    assert_equal Conversation.all, assigns(:conversations)
+    assert_equal Conversation.count, assigns(:conversations).length
+  end
+
+  test "raises RecordNotFound when not found" do
+    assert_raises(ActiveRecord::RecordNotFound) do
+      get '/conversations/123'
+    end
   end
 
 end
--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
 
 class MessagesControllerTest < ActionDispatch::IntegrationTest
-  # test "should get create" do
-  #   get messages_create_url
-  #   assert_response :success
+  test "should get error missing param" do
+    assert_raises(ActionController::ParameterMissing) do
+      post '/messages'
+    end
+  end
+  # test "should get work" do
+  #   post '/messages', params: { message: "new message", conversation_id: "id" }
   # end
 
 end

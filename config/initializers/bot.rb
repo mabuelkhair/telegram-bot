4a107b91b1d92ba1b410b9aae1dd1214ee96cc91
Mostafa Abu El-Khair <mabuelkhair94@gmail.com>
2020-03-21 23:06:22 +0200

    run initialzer if not test env

--- config/initializers/bot.rb
+++ config/initializers/bot.rb
@@ -1,14 +1,14 @@
 require 'telegram/bot'
+unless Rails.env.test?
+  token = Rails.application.credentials.telegram[:bot_token]
+  Telegram::Bot::Client.run(token) do |bot|
+    response = bot.api.getMe()
+    if ActiveRecord::Base.connection.table_exists? Sender.table_name
+      sender = Sender.find_or_create_by(id: response["result"]["id"],
+        first_name: response["result"]["first_name"])
+      BOT_ID = sender.id
+      # TODO: handle old messages and conversation if new bot used
+    end
 
-token = Rails.application.credentials.telegram[:bot_token]
-
-Telegram::Bot::Client.run(token) do |bot|
-  response = bot.api.getMe()
-  if ActiveRecord::Base.connection.table_exists? Sender.table_name
-    sender = Sender.find_or_create_by(id: response["result"]["id"],
-      first_name: response["result"]["first_name"])
-    BOT_ID = sender.id
-    # TODO: handle old messages and conversation if new bot used
   end
-
 end

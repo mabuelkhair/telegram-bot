a77fad5fe5445e52056730184a556e819bbefc91
Mostafa Abu El-Khair <mabuelkhair94@gmail.com>
2020-03-21 23:04:56 +0200

    move Message create to controller instead of service

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,16 @@
 class MessagesController < ApplicationController
+  before_action :validate_presence
+
   def create
-    Telegram::SenderService.call(params.dig(:conversation_id), params.dig(:message))
+    response = Telegram::SenderService.call(params.dig(:conversation_id), params.dig(:message))
+    Message.create(id: response["result"]["message_id"],
+      sender_id: response["result"]["from"]["id"],
+      conversation_id: params.dig(:conversation_id), content: params.dig(:message))
+  end
+
+  private
+  def validate_presence
+    params.require(:conversation_id)
+    params.require(:message)
   end
 end
--- app/services/telegram/sender_service.rb
+++ app/services/telegram/sender_service.rb
@@ -11,12 +11,10 @@ class Telegram::SenderService < ApplicationService
   def call
     token = Rails.application.credentials.telegram[:bot_token]
     Telegram::Bot::Client.run(token) do |bot|
-      response = bot.api.send_message(chat_id: chat_id, text: message)
+      @response = bot.api.send_message(chat_id: chat_id, text: message)
       # Todo: handle exceptions in message sending
-      Message.create(id: response["result"]["message_id"],
-        sender_id: response["result"]["from"]["id"],
-        conversation_id: chat_id, content: message)
     end
+    @response
   end
 
 end
